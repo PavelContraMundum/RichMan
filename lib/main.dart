7c3be2f862fd6a482f45cb46fc2e96d0358cbbec
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-21 22:00:20 +0100

    scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://pbs.twimg.com/profile_images/1310512844954370048/W5NEr-3W_400x400.jpg'),
+          ),
+        ),
       ),
     ),
   );
